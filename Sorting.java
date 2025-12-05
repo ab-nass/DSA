d85ccd0148e32a8da4cf48cc77a2d257c3f1a415
Abinash rath <rathabinash361@gmail.com>
2025-12-05 14:26:46 +0000

    insertion sort

--- Sorting.java
+++ Sorting.java
@@ -21,6 +21,9 @@ public class Sorting {
     System.out.println();
  }
   
+
+ // selection sort
+
  public static void Selectionsort(int arr[]) {
     for(int i=0; i < arr.length-1; i ++) {
         int minpos = i;
@@ -37,13 +40,26 @@ public class Sorting {
 
 
 
+ // insertionSort 
+
+       public static void insertionSort(int arr[]) {
+         for(int i=0; i < arr.length; i++) {
+            int curr = arr[i];
+            int prev = i -1;
+
+            while (prev >=0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
+                prev --;
+            }
+            arr[prev+1] = curr;
+         }
+       }
 
 
-
-    public static void main(String[] args) {
+ public static void main(String[] args) {
         int arr[] = {5, 4, 3, 2, 1};
         
-        Selectionsort(arr);
+        insertionSort(arr);
         printArr(arr);
     }
     
